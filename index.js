22472e2b40003a9d041d769f0eb6c8e474b82d75
Christophe pasquier <christof.pasquier@gmail.com>
2017-07-13 20:17:49 +0200

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const ul = require("ul")
+const fs = require("fs")
+const imageToAscii = require("image-to-ascii")
+const svg2png = require("svg2png")
+const { shape1, shape2 } = require('./shape')
+
+const imgToSvg = (path, opts, options, cb) => {
+    if (typeof opts === "function") {
+        options = {}
+        cb = opts;
+        opts = {};
+    }
+
+    if (typeof options === "function") {
+        cb = options;
+        options = {};
+        opts = {};
+    }
+
+    opts = ul.merge(opts, {
+        pxSize: 10
+      , margin: 2
+    });
+    let outerSize = opts.pxSize + opts.margin;
+
+    options = ul.deepMerge(options, {
+        size_options: {
+            px_size: {
+                width: 1
+            }
+        }
+      , stringify_fn: pixels => pixels.map((row, y) =>
+            row.map((pixel, x) => {
+                console.log(pixel);
+                return `<rect x="${x * outerSize}" y="${y * outerSize}" width="${opts.pxSize}" height="${opts.pxSize}" fill="rgb(${pixel.r}, ${pixel.g}, ${pixel.b})"></rect>`
+                const shapeFunc = shape1 //find random
+                const innerShape = shapeFunc(`rgba(${pixel.r}, ${pixel.g}, ${pixel.b})`)
+                const shape = `<rect x="${x * outerSize}" y="${y * outerSize}" width="${opts.pxSize}" height="${opts.pxSize}">${innerShape}</rect>`
+                return shape
+              }
+            ).join("\n")
+        ).join("\n")
+    });
+
+
+    imageToAscii(path, options, (err, converted, raw, img) => {
+        if (err) { return cb(err); }
+        // console.log(converted)
+        cb(null, `<svg width="${img[0].length * outerSize}" height="${img.length * outerSize}">${converted}</svg>`);
+    });
+};
+
+imgToSvg("https://cdn4.iconfinder.com/data/icons/proglyphs-free/512/Invader_3-24.png", (err, out) => {
+    // console.log(err || out);
+    fs.writeFileSync('lol.svg', out)
+
+    const svg = fs.readFileSync("lol.svg")
+    const png = svg2png(svg)
+    fs.writeFileSync('lol.png', png)
+});
---
+++ shape.js
@@ -0,0 +1,5 @@
+module.exports.shape1 = (color) =>
+  `<path fill="${color}" d="M443,248 L507,248 L507,312 C471.653776,312 443,283.346224 443,248 L443,248 Z M471,248 L507,248 L507,284 C487.117749,284 471,267.882251 471,248 L471,248 Z" ></path>`
+
+  module.exports.shape2 = (color) =>
+    `<g id="Page-1" stroke="none" stroke-width="1" fill="${color}" fill-rule="evenodd"><g id="slide" transform="translate(-443.000000, -248.000000)" fill="#00E9C2"><path d="M443,248 L507,248 L507,312 C471.653776,312 443,283.346224 443,248 L443,248 Z M471,248 L507,248 L507,284 C487.117749,284 471,267.882251 471,248 L471,248 Z" id="emptyQuartRound-bl" transform="translate(475.000000, 280.000000) scale(-1, -1) translate(-475.000000, -280.000000) "></path></g></g>`
