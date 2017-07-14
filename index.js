0a050298b363489fd4f3dc5a003964cdb88af721
Christophe pasquier <christof.pasquier@gmail.com>
2017-07-14 13:28:01 +0200

    1st running version with cloest colors

--- index.js
+++ index.js
@@ -3,46 +3,66 @@
 const ul = require("ul")
 const fs = require("fs")
 const imageToAscii = require("image-to-ascii")
-// const svg2png = require("svg2png")
-const { shape1 } = require('./shape')
+var DeltaE = require('delta-e')
+var colorSpace = require('color-space')
+const { shapes } = require('./shape')
 
-const imgToSvg = (path, opts, options, cb) => {
-    if (typeof opts === "function") {
-        options = {}
-        cb = opts;
-        opts = {};
+const colors = [
+  [ 90, 211, 220 ],
+  [ 142, 235, 238 ],
+  [ 254, 206, 85 ],
+  [ 254, 229, 153 ],
+  [ 120, 137, 243 ],
+  [ 252, 74, 96 ],
+  [ 250, 182, 165 ],
+  [ 185, 232, 139 ],
+  [ 243, 235, 148 ],
+]
+
+const closestToColor = (baseColor) => {
+  let maxScore = 100
+  let finalColor = [255, 255, 255]
+
+  let baseColorLAB = colorSpace.rgb.lab(baseColor)
+  baseColorLAB = { L: baseColorLAB[0], A: baseColorLAB[1], B: baseColorLAB[2] } // deltaE format
+
+  colors.map((color) => {
+    let colorLAB = colorSpace.rgb.lab(color)
+    colorLAB = { L: colorLAB[0], A: colorLAB[1], B: colorLAB[2] } // deltaE format
+    const score = DeltaE.getDeltaE00(baseColorLAB, colorLAB)
+    if (score < maxScore) {
+      finalColor = color
+      maxScore = score
     }
+  })
 
-    if (typeof options === "function") {
-        cb = options;
-        options = {};
-        opts = {};
-    }
+  return finalColor
+}
 
-    opts = ul.merge(opts, {
-        pxSize: 10
-      , margin: 2
-    });
-    let outerSize = opts.pxSize + opts.margin;
+const imgToSvg = (path, cb) => {
 
-    options = ul.deepMerge(options, {
-        size_options: {
-            px_size: {
-                width: 1
-            }
+    const pxSize = 1
+    let outerSize = pxSize
+
+    const stringify_fn = pixels => pixels.map((row, y) => {
+      return row.map((pixel, x) => {
+        // Random shape
+        const shape = shapes[Math.floor(Math.random()*shapes.length)];
+        const baseColor = [ pixel.r, pixel.g, pixel.b ]
+        const color = pixel.a === 0 ? [255, 255, 255] : closestToColor(baseColor)
+        return shape(x * outerSize, y * outerSize, pxSize, pxSize, `rgb(${color[0]}, ${color[1]}, ${color[2]})`)
+      }).join("")
+    }).join("")
+
+
+    const options = {
+      size_options: {
+        px_size: {
+          width: 1
         }
-      , stringify_fn: pixels => pixels.map((row, y) =>
-            row.map((pixel, x) => {
-                console.log(pixel);
-                return `<rect x="${x * outerSize}" y="${y * outerSize}" width="${opts.pxSize}" height="${opts.pxSize}" fill="rgb(${pixel.r}, ${pixel.g}, ${pixel.b})"></rect>`
-                const innerShape = shape1
-                const shape = `<rect x="${x * outerSize}" y="${y * outerSize}" width="${opts.pxSize}" height="${opts.pxSize}" fill="rgb(${pixel.r}, ${pixel.g}, ${pixel.b})">${innerShape}</rect>`
-                return shape
-              }
-            ).join("\n")
-        ).join("\n")
-    });
-
+      },
+      stringify_fn,
+    }
 
     imageToAscii(path, options, (err, converted, raw, img) => {
         if (err) { return cb(err); }
@@ -51,11 +71,7 @@ const imgToSvg = (path, opts, options, cb) => {
     });
 };
 
-imgToSvg("https://cdn4.iconfinder.com/data/icons/proglyphs-free/512/Invader_3-24.png", (err, out) => {
-    // console.log(err || out);
-    fs.writeFileSync('toprocess.svg', out)
-
-    // const svg = fs.readFileSync("toprocess.svg")
-    // const png = svg2png(svg)
-    // fs.writeFileSync('toprocess.png', png)
+imgToSvg("toprocess.png", (err, out) => {
+  fs.writeFileSync('result.svg', out)
+  console.log(err || 'success');
 });
--- shape.js
+++ shape.js
@@ -1,1 +1,55 @@
-module.exports.shape1 = (color) =>`<svg width="10px" height="10px" viewBox="0 0 10 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><polygon id="triangle-tl" fill="#FF6375" fill-rule="nonzero" points="10 0 0 10 0 0"></polygon><g id="1"></g></g></svg>`
+//triangles
+const shape1 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><polygon fill="${color}" fill-rule="nonzero" points="1 0 0 1 0 0"></polygon></svg>`
+
+const shape2 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><polygon fill="${color}" fill-rule="nonzero" transform="translate(0.500000, 0.500000) rotate(180.000000) translate(-0.500000, -0.500000) " points="1 0 0 1 0 0"></polygon></svg>`
+
+const shape3 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><polygon fill="${color}" fill-rule="nonzero" transform="translate(0.500000, 0.500000) rotate(-180.000000) translate(-0.500000, -0.500000) " points="1 0 1 1 0 0"></polygon></svg>`
+
+const shape4 = (x, y, width, height, color) =>
+`<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><polygon fill="${color}" fill-rule="nonzero"  transform="translate(0.500000, 0.500000) rotate(-180.000000) translate(-0.500000, -0.500000) " points="0 0 0 1 1 1"></polygon></svg>`
+
+//quarter rounds
+const shape5 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M1,0 C1,0.55228475 0.55228475,1 0,1 L0,0 L1,0 Z" style="mix-blend-mode: multiply;"></path></g></svg>`
+
+const shape6 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M1,0 C1,0.55228475 0.55228475,1 0,1 L0,0 L1,0 Z" style="mix-blend-mode: multiply;" transform="translate(0.500000, 0.500000) rotate(180.000000) translate(-0.500000, -0.500000) "></path></g></svg>`
+
+const shape7 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M1,0 C1,0.55228475 0.55228475,1 0,1 L0,0 L1,0 Z" style="mix-blend-mode: multiply;" transform="translate(0.500000, 0.500000) scale(-1, 1) translate(-0.500000, -0.500000) "></path></g></svg>`
+
+const shape8 = (x, y, width, height, color) =>
+`<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M1,0 C1,0.55228475 0.55228475,1 0,1 L0,0 L1,0 Z" style="mix-blend-mode: multiply;" transform="translate(0.500000, 0.500000) scale(-1, 1) rotate(-180.000000) translate(-0.500000, -0.500000) "></path></g></svg>`
+
+//quarter empty rounds
+const shape9 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M1,0.00115749473 C0.997329712,0.501941234 0.603286743,0.997467041 0.00115749473,1 L-3.60822483e-15,0.5 C0.270874023,0.500578747 0.5,0.290740967 0.500578747,0.00115749473 L1,0.00115749473 Z" id="Path"></path></g></svg>`
+
+const shape10 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M-2.94209102e-15,1 C0.00267028809,0.499216261 0.396713257,0.00369045371 0.998842505,0.00115749473 L1,0.501157495 C0.729125977,0.500578747 0.5,0.710416528 0.499421253,1 L-2.94209102e-15,1 Z" id="Path"></path></g></svg>`
+
+const shape11 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M-2.94209102e-15,0 C0.00267028809,0.500783739 0.396713257,0.996309546 0.998842505,0.998842505 L1,0.498842505 C0.729125977,0.499421253 0.5,0.289583472 0.499421253,2.10942375e-14 L-2.94209102e-15,0 Z" id="Path"></path></g></svg>`
+
+const shape12 = (x, y, width, height, color) =>
+`<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M1,1 C0.997329712,0.499216261 0.603286743,0.00369045371 0.00115749473,0.00115749473 L-2.94209102e-15,0.501157495 C0.270874023,0.500578747 0.5,0.710416528 0.500578747,1 L1,1 Z" id="Path"></path></g></svg>`
+
+//others
+const shape13 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><polygon points="0 0 1 0 1 1 0 1"></polygon></g></svg>`
+
+const shape14 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M0.5,0 C0.776142375,0 1,0.223857625 1,0.5 C1,0.776142375 0.776142375,1 0.5,1 C0.223857625,1 0,0.776142375 0,0.5 C0,0.223857625 0.223857625,0 0.5,0 Z M0.5,0.25 C0.361928812,0.25 0.25,0.361928812 0.25,0.5 C0.25,0.638071188 0.361928812,0.75 0.5,0.75 C0.638071188,0.75 0.75,0.638071188 0.75,0.5 C0.75,0.361928812 0.638071188,0.25 0.5,0.25 Z" id="emptyround"></path></g></svg>`
+
+const shape15 = (x, y, width, height, color) =>
+  `<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><path d="M0.5,0 C0.776142375,0 1,0.223857625 1,0.5 C1,0.776142375 0.776142375,1 0.5,1 C0.223857625,1 0,0.776142375 0,0.5 C0,0.223857625 0.223857625,0 0.5,0 Z M0.5,0.25 C0.361928812,0.25 0.25,0.361928812 0.25,0.5 C0.25,0.638071188 0.361928812,0.75 0.5,0.75 C0.638071188,0.75 0.75,0.638071188 0.75,0.5 C0.75,0.361928812 0.638071188,0.25 0.5,0.25 Z" id="emptyround"></path></g></svg>`
+
+const shape16 = (x, y, width, height, color) =>
+`<svg x=${x} y=${y} width=${width} height=${height} viewBox="0 0 1 1"><g fill="${color}"><circle cx="0.5" cy="0.5" r="0.5"></circle></g></svg>`
+
+
+
+module.exports.shapes = [shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape8, shape9, shape10, shape11, shape12, shape13, shape14, shape15, shape16]
